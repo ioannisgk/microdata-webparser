54bcc1c4bd5792dfb61d787cc01e820216eab6c6
Ioannis Gkourtzounis <ioannisgk@live.com>
2018-01-06 13:56:52 +0200

    Update ParseMode2Service.java

--- src/com/ioannisgk/microdatawebparser/services/ParseMode2Service.java
+++ src/com/ioannisgk/microdatawebparser/services/ParseMode2Service.java
@@ -12,31 +12,47 @@ import org.springframework.stereotype.Service;
 public class ParseMode2Service {
 	
 	public HashMap parse(Document document) {
+		
+		// Arraylists that contain actual microdata
 		ArrayList<String> schemaKeywords = new ArrayList<String>();
 		ArrayList<String> schemaPathophysiology = new ArrayList<String>();
 		ArrayList<String> schemaTreatment = new ArrayList<String>();
+		
 		String keywords = "", pathophysiology = "", treatment = "";
 		HashMap extractedCondition = new HashMap();
 		
+		// Select itemprop and iterate document element
 		Elements elements = document.select("*[itemprop]");
 		for (Element el:elements){
-						
+					
+			// Save only valid data
 			if (el.attr("itemprop").equals("name") || el.attr("itemprop").equals("relevantSpecialty")) {
+				
+				// Add valid data to arraylist
 				schemaKeywords.add(el.text());
 			}
 			
+			// Add valid data to arraylist
 			if (el.attr("itemprop").equals("pathophysiology")) schemaPathophysiology.add(el.text());
 			if (el.attr("itemprop").equals("possibleTreatment")) schemaTreatment.add(el.text());
 		}
+		
+		// Concatenate keywords
 		for (int i = 1; i < schemaKeywords.size(); i++) {
 			keywords = keywords + " " + schemaKeywords.get(i);
 		}
+		
+		// Concatenate pathophysiology
 		for (int i = 0; i < schemaPathophysiology.size(); i++) {
 			pathophysiology = pathophysiology + " " + schemaPathophysiology.get(i);
 		}
+		
+		// Concatenate treatment
 		for (int i = 0; i < schemaTreatment.size(); i++) {
 			treatment = treatment + " " + schemaTreatment.get(i);
 		}
+		
+		// Update hashmaps with microdata properties and values
 		extractedCondition.put("Alternate Name", schemaKeywords.get(0));
 		extractedCondition.put("Keywords", (keywords.isEmpty()) ? keywords : keywords.substring(1));
 		extractedCondition.put("Pathophysiology", (pathophysiology.isEmpty()) ? pathophysiology : pathophysiology.substring(1));		
